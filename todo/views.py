3ca5d314a15b17d6c48ce95fea8314c86f793fb5
Sandesh-Salunkhe <sandesh151099@gmail.com>
2023-05-16 14:49:24 +0530

    rask_new

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from .models import *
-from django.http import Http404
+from django.http import Http404, HttpResponse, JsonResponse
 
 @login_required(login_url='/login/')
 def todo_index(request):
@@ -14,7 +14,6 @@ def todo_index(request):
         data = Task_List()
         data.user = request.user
         data.task_title = task_name
-        
         data.save()
 
     task_list = Task_List.objects.filter(user=request.user)
@@ -22,6 +21,7 @@ def todo_index(request):
     context = {
         'data': task_list
     }
+
     return render(request, 'html/todo/todo_index.html', context)
 
 # @login_required(login_url='/login/')
@@ -44,8 +44,20 @@ def todo_index(request):
 
 @login_required(login_url='/login/')
 def todo_edit(request):
-
     slug = request.GET["pk"]
+
+    if request.method == 'POST':
+        # print(list(request.POST.items()))
+        task_name = request.POST.get('task_update')
+        status_code = request.POST.get('status_code')
+        description = request.POST.get('description')
+        
+        data = Tasks()
+        data.task_list = request.user
+        data.task_name = task_name
+        data.description = description
+        data.status_code = status_code
+        data.save()
 
     try:
         task_id = Task_List.objects.get(id = slug, user = request.user)
